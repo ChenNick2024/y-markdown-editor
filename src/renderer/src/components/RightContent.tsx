0d78ff3b1dbd8659ed68a15fb2151e80b0361551
Nick930826 <xianyou1993@qq.com>
2025-01-07 22:00:37 +0800

    添加页签

--- src/renderer/src/components/RightContent.tsx
+++ src/renderer/src/components/RightContent.tsx
@@ -1,11 +1,48 @@
+import { useState } from 'react'
+import { Tabs } from 'antd'
 import { useStore } from '@renderer/store'
 
 function RightContent(): JSX.Element {
   // const title = useStore((state: any) => state.title)
-
+  const [activeKey, setActiveKey] = useState('1')
+  const [items, setItems] = useState([
+    {
+      label: '地洒到卡上的疯狂打扫',
+      key: '1'
+    },
+    {
+      label: 'Tab 2',
+      key: '2'
+    },
+    {
+      label: 'Tab 3',
+      key: '3'
+    }
+  ])
   return (
-    <div className="h-full overflow-hidden bg-pink-200">
-      <div>xxx</div>
+    <div className="h-full overflow-hidden bg-white border-l border-[#f5f5f5]">
+      <div className="h-full p-4">
+        {items.length ? (
+          <div className="tab-wrap">
+            <Tabs
+              type="editable-card"
+              onChange={(key) => {
+                setActiveKey(key)
+              }}
+              hideAdd
+              activeKey={activeKey}
+              onEdit={(targetKey, action) => {
+                console.log(targetKey, action)
+                if (action === 'remove') {
+                  console.log(targetKey)
+                  setItems(items.filter((item) => item.key != targetKey))
+                }
+              }}
+              items={items}
+            />
+          </div>
+        ) : null}
+      </div>
     </div>
   )
 }
