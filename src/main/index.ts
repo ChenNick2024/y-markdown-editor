72e8ad5f22d623d14f843863ee8b96a514c40d69
Nick930826 <xianyou1993@qq.com>
2025-01-08 15:01:06 +0800

    up

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,7 +2,7 @@
  * @Author: Nick930826 xianyou1993@qq.com
  * @Date: 2025-01-05 18:20:44
  * @LastEditors: Nick930826 xianyou1993@qq.com
- * @LastEditTime: 2025-01-08 14:20:41
+ * @LastEditTime: 2025-01-08 15:00:58
  * @FilePath: /y-markdown-editor/src/main/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -126,7 +126,6 @@ app.whenReady().then(() => {
 
   ipcMain.handle('get-current-article', async (_event, article) => {
     const content = fs.readFileSync(`${article.filePath}/${article.title}.md`, 'utf-8')
-    console.log('content', content)
     return { code: 0, content }
   })
 
