ec16b70219a725add810aad6feb03075e0d793c0
Nick930826 <xianyou1993@qq.com>
2025-01-09 10:15:49 +0800

    up

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -2,7 +2,7 @@
  * @Author: Nick930826 xianyou1993@qq.com
  * @Date: 2025-01-06 09:17:11
  * @LastEditors: Nick930826 xianyou1993@qq.com
- * @LastEditTime: 2025-01-09 10:05:03
+ * @LastEditTime: 2025-01-09 10:15:42
  * @FilePath: /y-markdown-editor/src/renderer/src/store/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,7 +15,7 @@ import dayjs from 'dayjs'
 export const useStore = create<StoreProps>((set) => ({
   // 文章相关
   articles: getArticles(),
-  activeArticle: getArticles()[0] || {},
+  activeArticle: {} as ArticleProps,
   setActiveArticle: async (article: ArticleProps): Promise<void> => {
     const res = await window.electron.ipcRenderer.invoke('get-current-article', article)
     article.content = res.content
